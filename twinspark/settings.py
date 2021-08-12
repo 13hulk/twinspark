058ac64214c0fad24eae32c23cfcbe29c3e1c85d
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-08-12 14:08:28 +0530

    add test case twinspark app version

--- twinspark/settings.py
+++ twinspark/settings.py
@@ -14,11 +14,11 @@ from pathlib import Path
 
 from environs import Env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = Env()
 env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
