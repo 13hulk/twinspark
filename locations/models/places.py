6c56921b34f157ddbaa7f5c0965394fa31322a66
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-08-12 17:41:26 +0530

    add new django model - Place; configure postgres db

---
+++ locations/models/places.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Place(models.Model):
+    name = models.CharField(
+        "Place name",
+        max_length=1024,
+    )
+    address = models.CharField("Address", max_length=2014, null=True)
+    city = models.CharField("City", max_length=1024)
+    district = models.CharField("District", max_length=1024)
+    state = models.CharField("State", max_length=1024)
+    country = models.CharField("Country", max_length=1024)
+    zip = models.CharField(
+        "ZIP / Postal code",
+        max_length=6,
+    )
+    # coordinates = gid_models.PointField()
